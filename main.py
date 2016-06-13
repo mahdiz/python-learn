1bfed55d0d9868d713feb0227067565650b679dc
Mahdi Zamani <mahdi.zamani@yale.edu>
2016-06-13 15:35:38 -0400

    Dictionary example added

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 def input_example():
     a = raw_input()
-    s = a.split(' ')
-    print s
+    print a.split(' ')
 
 
 def loop_example():
     for x in range(1, 11):
         print x
+
+    # Iterating over a string
+    for c in "Hello world!":
+        print c
 
     count = 0
     while count < 5:
@@ -27,6 +30,11 @@ def string_example():
     print 'a'*5       # Repeat 'a' 5 times
     print len(word)
 
+    x = " test "
+    print x.strip()
+    x = "0023.57000"
+    print x.strip('0')
+
     # print('We are the {} who say "{}!"'.format('knights', 'Ni'))
 
 
@@ -40,7 +48,8 @@ def lists_example():
     b = [8, 9, 5, 3, 7, 1, 2, 0]
     print filter(lambda x: x % 2 == 0, a)   # Filter based on a condition
     print map(lambda x: x**2, a)            # Using map
-    print map(lambda x,y: x+y, a, b)        # Map over two arrays
+    print map(lambda x, y: x+y, a, b)        # Map over two arrays
+    print map(int, ['56', '3', '14'])        # Map cast
 
     # Sorting an array
     c = [(3, 'three'), (1,'one'), (4, 'four'), (2,'two')]
@@ -55,5 +64,29 @@ def lists_example():
     s = (x**2 for x in [1, 2, 3, 4, 5])
     print list(s)
 
+    # Zip two arrays to create pairs
+    firsts = ['a', 'b', 'c']
+    seconds = [4, 5, 6]
+    print zip(firsts, seconds)
+
+
+def dictionary_example():
+    D = {}      # Empty dictionary
+    D['a'] = 1
+    D['b'] = 2
+    D['c'] = 3
+
+    print D
+
+    for k in D.keys():
+        print k, '->', D[k]
+
+    for v in D.values():
+        print v
+
+    for k,v in D.items():
+        print k, '->', v
+
+
 if __name__ == '__main__':
-    lists_example()
+    loop_example()
